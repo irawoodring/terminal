ae8ace5ffbed8f6abe3558e5678b231550191e2c
Ira Woodring <irawoodring@gmail.com>
2026-09-11 21:21:49 -0400

    Fix spacing.

--- src/lib/GameOverlay.svelte
+++ src/lib/GameOverlay.svelte
@@ -5,7 +5,7 @@ cxStore.subscribe(v => cx = v);
 termStore.subscribe(v => term = v);
 
 async function checkLevel1() {
-  if (!cx) return false;
-  const exitCode = await cx.run('/bin/sh', ['-c', 'test -f /home/user/flag.txt']);
-  return exitCode === 0;
+    if (!cx) return false;
+    const exitCode = await cx.run('/bin/sh', ['-c', 'test -f /home/user/flag.txt']);
+    return exitCode === 0;
 }
