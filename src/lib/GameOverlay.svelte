03c11d66c2d0cb51c14077cb7de544c4ce7aeb45
Ira Woodring <irawoodring@gmail.com>
2026-09-11 22:34:31 -0400

    Update quotes.

--- src/lib/GameOverlay.svelte
+++ src/lib/GameOverlay.svelte
@@ -7,7 +7,7 @@
 
   async function checkLevel1() {
     if (!cx) return false;
-    const exitCode = await cx.run('/bin/sh', ['-c', 'test -f /home/user/flag.txt']);
+    const exitCode = await cx.run("/bin/sh", ["-c", "test -f /home/user/flag.txt"]);
     return exitCode === 0;
   }
 </script>
