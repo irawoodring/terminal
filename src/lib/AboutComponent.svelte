8ac24aa5f555dc4d75612af805ff12c185ed1967
Ira Woodring <irawoodring@gmail.com>
2026-09-16 19:04:21 -0400

    Update CSS.

--- src/lib/AboutComponent.svelte
+++ src/lib/AboutComponent.svelte
@@ -4,12 +4,12 @@
 </script>
 
 <div class="backdrop" on:click={() => dispatch('close')}>
-  <div class="panel flex justify-center" on:click|stopPropagation>
+  <div class="panel flex" on:click|stopPropagation>
     <p>Terminal is a game for learning the command line (bash).  It runs on a virtual machine in your browser which runs a Linux distro. Progress is not saved, so refreshing your browser may result in loss of progress.  However, this also means if you mess something up, you can easily refresh the system to the starting state.</p>
     <p>This project runs off of <a href="https://webvm.io/">WebVM</a>.</p>
     <br />
     &copy;2026
-    <button class="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded font-bold" on:click={() => dispatch('close')}>Close</button>
+    <button class="block mx-auto bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded font-bold" on:click={() => dispatch('close')}>Close</button>
   </div>
 </div>
 
