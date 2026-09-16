e007ffe934ae74f74e89eda058ed58bddfb094f3
Ira Woodring <irawoodring@gmail.com>
2026-09-16 16:07:35 -0400

    Adjust CSS.

--- src/lib/AboutComponent.svelte
+++ src/lib/AboutComponent.svelte
@@ -5,10 +5,7 @@
 
 <div class="backdrop" on:click={() => dispatch('close')}>
   <div class="panel" on:click|stopPropagation>
-    <p>Terminal is a game for learning bash.  It runs on a virtual machine in your browser
-       which runs a Linux distro. Progress is not saved, so refreshing your browser may
-       result in loss of progress.  This also means if you mess somethign up, you can
-       easily refresh the system to the starting state.</p>
+    <p>Terminal is a game for learning the command line (bash).  It runs on a virtual machine in your browser which runs a Linux distro. Progress is not saved, so refreshing your browser may result in loss of progress.  This also means if you mess somethign up, you can easily refresh the system to the starting state.</p>
     <button on:click={() => dispatch('close')}>Close</button>
   </div>
 </div>
@@ -22,6 +19,10 @@
     display: flex;
     align-items: center;
     justify-content: center;
+    margin: 1em;
+  }
+  .button {
+    background-color: green;
   }
   .panel {
     background: white;
